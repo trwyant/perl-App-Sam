1c5f45af2f4174c60ab6a6d2b3482c898f5d7891
Tom Wyant <wyant@cpan.org>
2024-01-13 17:31:59 -0500

    Update copyright to 2024.

--- t/data/cpp_file.cpp
+++ t/data/cpp_file.cpp
@@ -19,7 +19,7 @@ int main( int argc, char *argv[] ) {
 /*
  * Author: Thomas R. Wyant, III F<wyant at cpan dot org>
  *
- * Copyright (C) 2018-2023 by Thomas R. Wyant, III
+ * Copyright (C) 2018-2024 by Thomas R. Wyant, III
  *
  * This program is distributed in the hope that it will be useful, but
  * without any warranty; without even the implied warranty of
